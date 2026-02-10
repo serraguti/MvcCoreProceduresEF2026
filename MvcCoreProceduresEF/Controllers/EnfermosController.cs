1b8ed889fd56c6c108bf225022c051f927659f77
serraguti <pacoserranox@gmail.com>
2026-02-10 14:20:34 +0100

    Add project files.

---
+++ MvcCoreProceduresEF/Controllers/EnfermosController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreProceduresEF.Models;
+using MvcCoreProceduresEF.Repositories;
+
+namespace MvcCoreProceduresEF.Controllers
+{
+    public class EnfermosController : Controller
+    {
+        private RepositoryEnfermos repo;
+
+        public EnfermosController(RepositoryEnfermos repo)
+        {
+            this.repo = repo;
+        }
+
+        public async Task< IActionResult> Index()
+        {
+            List<Enfermo> enfermos = await this.repo.GetEnfermosAsync();
+            return View(enfermos);
+        }
+    }
+}
---
+++ MvcCoreProceduresEF/Repositories/RepositoryEnfermos.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcCoreProceduresEF.Data;
+using MvcCoreProceduresEF.Models;
+using System.Data;
+using System.Data.Common;
+
+namespace MvcCoreProceduresEF.Repositories
+{
+    #region STORED PROCEDURES
+    /*
+    create procedure SP_ALL_ENFERMOS
+    as
+	    select * from ENFERMO
+    go
+    create procedure SP_FIND_ENFERMO
+    (@inscripcion nvarchar(50))
+    as
+	    select * from ENFERMO
+	    where INSCRIPCION=@inscripcion
+    go
+    create procedure SP_DELETE_ENFERMO
+    (@inscripcion nvarchar(50))
+    as
+	    delete from ENFERMO where INSCRIPCION=@inscripcion
+    go
+     */
+    #endregion
+
+    public class RepositoryEnfermos
+    {
+        private EnfermosContext context;
+
+        public RepositoryEnfermos(EnfermosContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Enfermo>> GetEnfermosAsync()
+        {
+            //NECESITAMOS UN COMMAND, VAMOS A UTILIZAR UN using
+            //PARA TODO
+            //EL COMMAND, EN SU CREACION, NECESITA DE UNA CADENA DE 
+            //CONEXION (OBJETO)
+            //EL OBJETO CONNECTION NOS LO OFRECE EF
+            //las conexiones se crean a partir del context
+            using (DbCommand com =
+                this.context.Database.GetDbConnection().CreateCommand())
+            {
+                string sql = "SP_ALL_ENFERMOS";
+                com.CommandType = CommandType.StoredProcedure;
+                com.CommandText = sql;
+                //ABRIMOS LA CONEXION A PARTIR DEL COMMAND
+                await com.Connection.OpenAsync();
+                //EJECUTAMOS NUESTRO READER
+                DbDataReader reader = await com.ExecuteReaderAsync();
+                //DEBEMOS MAPEAR LOS DATOS MANUALMENTE
+                List<Enfermo> enfermos = new List<Enfermo>();
+                while (await reader.ReadAsync())
+                {
+                    Enfermo enfermo = new Enfermo
+                    {
+                        Inscripcion = reader["INSCRIPCION"].ToString(),
+                        Apellido = reader["APELLIDO"].ToString(),
+                        Direccion = reader["DIRECCION"].ToString(),
+                        FechaNacimiento = DateTime.Parse
+                        (reader["FECHA_NAC"].ToString()),
+                        Genero = reader["S"].ToString(),
+                        Nss = reader["NSS"].ToString()
+                    };
+                    enfermos.Add(enfermo);
+                }
+                await reader.CloseAsync();
+                await com.Connection.CloseAsync();
+                return enfermos;
+            }
+        }
+    }
+}
